4e2506aef282f95a49f422f6ce27c93579656c9c
Aditya Hajare <aditya43@gmail.com>
2018-03-08 09:03:23 +0530

    Create Thread and Reply model with resource controller and factories

---
+++ database/factories/ThreadFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Thread::class, function (Faker $faker)
+{
+    return [
+        'user_id' => function ()
+        {
+            return factory(App\User::class)->create()->id;
+        },
+        'title'   => $faker->sentence(10),
+        'body'    => $faker->paragraph(3)
+    ];
+});
