87e76c0b870c1871a9b9467de4b5115367002975
Aditya Hajare <aditya43@gmail.com>
2018-03-09 16:34:24 +0530

    Add channels and associate threads to channels

--- app/Reply.php
+++ app/Reply.php
@@ -1,15 +1,23 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    protected $fillable = ['body', 'user_id'];
-
+    /**
+     * Don't auto-apply mass assignment protection.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+    /**
+     * A reply has an owner.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function owner()
     {
-        return $this->belongsTo(\App\User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
 Route::get('/', function ()
 {
     return view('welcome');
@@ -20,6 +9,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('threads', 'ThreadsController');
+// Route::resource('threads', 'ThreadsController');
+Route::get('threads', 'ThreadsController@index');
+Route::get('threads/create', 'ThreadsController@create');
+Route::get('threads/{channel}/{thread}', 'ThreadsController@show');
 
-Route::post('/threads/{thread}/replies', 'RepliesController@store');
+Route::post('/threads', 'ThreadsController@store');
+
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
