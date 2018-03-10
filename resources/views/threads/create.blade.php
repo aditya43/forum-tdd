2b30621500f99a9a6427b6ef2366311d143a94c9
Aditya Hajare <aditya43@gmail.com>
2018-03-10 08:34:03 +0530

    Persist old form data and add validation errors to forms

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -10,15 +10,27 @@
                 <div class="card-body">
                     <form method="POST" action="/threads">
                         @csrf
+
+                        <div class="form-group">
+                            <label for="channel_id">Choose a Channel:</label>
+                                <select class="form-control {{ $errors->has('channel_id') ? 'is-invalid' : ''}}" id="channel_id" name="channel_id" required>
+                                    <option value="" disabled selected>Choose One..</option>
+                                    @foreach (App\Channel::all() as $channel)
+                                        <option value="{{ $channel->id }}" {{ old('channel_id') == $channel->id ? 'selected' : '' }}>{{ $channel->name }}</option>
+                                    @endforeach
+                                </select>
+                                <div class="invalid-feedback">{{ $errors->first('channel_id') }}</div>
+                        </div>
+
                         <div class="form-group">
                             <label for="title">Title:</label>
-                            <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}" name="title" id="title" value="{{ old('title') }}">
+                            <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : ''}}" name="title" id="title" value="{{ old('title') }}" required>
                             <div class="invalid-feedback">{{ $errors->first('title') }}</div>
                         </div>
 
                         <div class="form-group">
                             <label for="body">Body:</label>
-                            <textarea name="body" id="body" class="form-control" rows="10" {{ $errors->has('body') ? 'is-invalid' : ''}}>{{ old('body') }}</textarea>
+                            <textarea name="body" id="body" class="form-control {{ $errors->has('body') ? 'is-invalid' : ''}}" rows="10" {{ $errors->has('body') ? 'is-invalid' : ''}} required>{{ old('body') }}</textarea>
                             <div class="invalid-feedback">{{ $errors->first('body') }}</div>
                         </div>
 
