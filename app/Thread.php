0220c4753ff8b6c84df86985a22858221ecf2bd4
Aditya Hajare <aditya43@gmail.com>
2018-03-17 09:14:58 +0530

    Refactor to events and event-listeners

--- app/Thread.php
+++ app/Thread.php
@@ -83,11 +83,7 @@ class Thread extends Model
     {
         $reply =  $this->replies()->create($reply);
 
-        $this->subscriptions
-            ->filter(function ($sub) use ($reply) {
-                return $sub->user_id != $reply->user_id;
-            })
-            ->each->notify($reply);
+        event(new \App\Events\ThreadHasNewReply($this, $reply));
 
         return $reply;
     }
